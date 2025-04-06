d4e86ae86508b313b2770adf7f81f2de8ce38f81
rothanak <thoeurnrothanak@gmail.com>
2025-04-06 23:18:05 +0700

    refactor: Remove unused Discord API routes and related components to streamline codebase

--- src/app/peachy/guilds/[guild]/page.tsx
+++ src/app/peachy/guilds/[guild]/page.tsx
@@ -1,32 +1,8 @@
 "use client";
 
-import { CardStack } from "@/components/bot-card";
-import { cn } from "@/lib/utils";
-import { CARD } from "@/utils/config";
-
-export const Highlight = ({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) => {
-  return (
-    <span
-      className={cn(
-        "font-bold bg-emerald-100 text-emerald-700 dark:bg-emerald-700/[0.2] dark:text-emerald-500 px-1 py-0.5",
-        className
-      )}
-    >
-      {children}
-    </span>
-  );
-};
-
 export default function FeaturePage() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center">
-      {/* <CardStack items={CARD} /> */}
       FEATURE PAGES
     </div>
   );
