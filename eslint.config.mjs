50445586c2b06490822622e557a5d1fe5678d4d1
rothanak <thoeurnrothanak@gmail.com>
2025-04-06 23:28:49 +0700

    refactor: Update ESLint configuration to disable specific rules, add new dependencies, and modify global styles for improved consistency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", // Disable unused variables rule
+      "@typescript-eslint/no-explicit-any": "off", // Allow 'any' type
+      "react/no-unescaped-entities": "off", // Allow unescaped entities in JSX
+    },
+  },
 ];
 
 export default eslintConfig;
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -113,7 +113,7 @@ export function toUpperCase(text: string): string {
 
 export function toRGB(num: number) {
   num >>>= 0;
-  let b = num & 0xff,
+  const b = num & 0xff,
     g = (num & 0xff00) >>> 8,
     r = (num & 0xff0000) >>> 16;
   return "rgb(" + [r, g, b].join(",") + ")";
