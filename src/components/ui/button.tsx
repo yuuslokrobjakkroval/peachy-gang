14dcfce72ebea90b4b8a6dd4a479468f0c75c5f4
rothanak <thoeurnrothanak@gmail.com>
2025-04-06 23:32:24 +0700

    feat: Add 'xl' variant for button and sidebar menu button styles to enhance responsiveness

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -25,6 +25,7 @@ const buttonVariants = cva(
         default: "h-10 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-12 rounded-md px-6 has-[>svg]:px-4",
+        xl: "h-16 rounded-md px-6 has-[>svg]:px-4",
         icon: "size-10",
       },
     },
