7b9f2fdb3e7f0f1769da2cac54f0ca2a80f7fa99
Yuu <108858022+yuuslokrobjakkroval@users.noreply.github.com>
2025-04-21 00:25:26 +0700

    Update route.ts

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -47,7 +47,7 @@ export async function GET(request: NextRequest) {
     const tokenData = (await tokenResponse.json()) as AccessToken;
 
     const response = NextResponse.redirect(
-      `${getAbsoluteUrl()}/peachy/dashboard`
+      `${getAbsoluteUrl()}/dashboard`
     );
 
     setCookie(TokenCookie, JSON.stringify(tokenData), {
