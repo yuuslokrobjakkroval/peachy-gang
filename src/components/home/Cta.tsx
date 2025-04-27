95596abd18999bbe5003aad970a967f600e0bb79
rothanak <thoeurnrothanak@gmail.com>
2025-04-27 22:21:14 +0700

    refactor: clean up formatting and indentation in Cta component

--- src/components/home/Cta.tsx
+++ src/components/home/Cta.tsx
@@ -5,34 +5,27 @@ import { Container } from "@/components/home/Container";
 import { Button } from "@/components/ui/button";
 
 export const Cta = () => {
-    const handleDiscordLogin = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-        const loginUrl = "/api/auth/login";
-        if (typeof window !== "undefined") {
-            window.location.href = loginUrl;
-        }
-    };
+  const handleDiscordLogin = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const loginUrl = "/api/auth/login";
+    if (typeof window !== "undefined") {
+      window.location.href = loginUrl;
+    }
+  };
 
-    return (
-        <Container>
-            <div
-                className="flex flex-wrap items-center justify-between w-full max-w-4xl gap-5 mx-auto text-primary-foreground bg-primary px-7 py-7 lg:px-12 lg:py-12 lg:flex-nowrap rounded-md shadow-sm border-border dark:bg-primary dark:text-primary-foreground"
-            >
-                <div className="flex-grow text-center lg:text-left">
-                    <h2 className="text-2xl font-ghibi-bold lg:text-3xl">
-                        Ready to try out this bot?
-                    </h2>
-                    <p className="mt-2 font-ghibi text-primary-foreground text-opacity-90 lg:text-xl dark:text-primary-foreground">
-                        Don't let your visitors see a poor bot.
-                    </p>
-                </div>
-                <Button
-                    onClick={handleDiscordLogin}
-                    className="inline-block text-center mx-auto text-lg font-ghibi-bold text-primary bg-primary-foreground rounded-md hover:bg-primary-foreground/90 dark:bg-primary-foreground dark:text-primary"
-                >
-                    Get Started
-                </Button>
-            </div>
-        </Container>
-    );
+  return (
+    <Container>
+      <div className="flex flex-wrap items-center justify-between w-full max-w-4xl gap-5 mx-auto text-primary-foreground bg-primary px-7 py-7 lg:px-12 lg:py-12 lg:flex-nowrap rounded-md shadow-sm border-border dark:bg-primary dark:text-primary-foreground">
+        <div className="flex-grow text-center lg:text-left">
+          <h2 className="text-2xl font-ghibi-bold lg:text-3xl">
+            Ready to try out this bot?
+          </h2>
+          <p className="mt-2 font-ghibi text-primary-foreground text-opacity-90 lg:text-xl dark:text-primary-foreground">
+            Don't let your visitors see a poor bot.
+          </p>
+        </div>
+        <Button onClick={handleDiscordLogin}>Get Started</Button>
+      </div>
+    </Container>
+  );
 };
