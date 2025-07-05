80c637b225f9112ba5d284cad68a09106669b1c7
yuuslokrobjakkroval <thoeurnrothanak@gmail.com>
2025-07-06 04:20:26 +0700

    fix: update dotenv import and configuration for environment variables

--- src/utils/get-absolute-url.ts
+++ src/utils/get-absolute-url.ts
@@ -1,8 +1,17 @@
-import { config } from "dotenv";
+import dotenv from "dotenv";
 
-config();
+dotenv.config();
 
 export function getAbsoluteUrl(): string {
   const defaultUrl = "http://localhost:3000";
+  console.log(process.env.APP_URL);
+
   return process.env.APP_URL ?? defaultUrl;
+}
+
+export function getBetterAuthUrl(): string {
+  const defaultUrl = "https://peachygang.xyz";
+  console.log(process.env.BETTER_AUTH_URL);
+
+  return process.env.BETTER_AUTH_URL ?? defaultUrl;
 }
