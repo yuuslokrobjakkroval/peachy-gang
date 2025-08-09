2d845efcec4ba1a185db7bcade1ebfe50b7db05e
yuuslokrobjakkroval <thoeurnrothanak@gmail.com>
2025-08-10 02:18:42 +0700

    fix: remove outdated sitemap and robots.txt files; update install command to include postbuild script

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteUrl: 'https://peachyganggg.com',
+  generateRobotsTxt: true, // Also generates robots.txt
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000, // Split sitemap if > 5000 URLs
+};
