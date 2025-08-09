4af373fccc2c5140f0d0bb583983195d0a2e794e
yuuslokrobjakkroval <thoeurnrothanak@gmail.com>
2025-08-10 01:48:16 +0700

    fix: remove next-sitemap configuration and update package.json scripts

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  siteUrl: "https://peachyganggg.com",
-  generateRobotsTxt: true,
-  sitemapSize: 5000,
-  changefreq: "daily",
-  priority: 0.7,
-  generateIndex: true,
-  outDir: "./public",
-};
