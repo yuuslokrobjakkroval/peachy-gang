41fb11ac649b3fbad27f93fa137fa61f41094b84
yuuslokrobjakkroval <thoeurnrothanak@gmail.com>
2025-08-10 00:57:32 +0700

    fix: add missing properties for sitemap generation in next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,9 @@
-// next-sitemap.config.js
 module.exports = {
   siteUrl: "https://peachyganggg.com",
   generateRobotsTxt: true,
   sitemapSize: 5000,
   changefreq: "daily",
   priority: 0.7,
+  generateIndex: true,
+  outDir: "./public",
 };
