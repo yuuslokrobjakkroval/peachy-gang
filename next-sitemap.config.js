5e230a5533394910c02c973397aab3186de5d33e
yuuslokrobjakkroval <thoeurnrothanak@gmail.com>
2025-08-10 17:03:05 +0700

    feat: implement new sitemap and robots.txt generation; remove outdated configurations

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteUrl: 'https://peachyganggg.com',
-  generateRobotsTxt: true, // Also generates robots.txt
-  changefreq: 'daily',
-  priority: 0.7,
-  sitemapSize: 5000, // Split sitemap if > 5000 URLs
-};
